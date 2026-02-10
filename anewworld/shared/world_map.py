32cc5cf92607b909afe6fd251ee9cc4e9e91e47c
Benjamin Horowitz <hhorowitz734@gmail.com>
2026-02-10 16:11:03 -0600

    began refactor of TileMap to WorldMap

--- anewworld/shared/world_map.py
+++ anewworld/shared/world_map.py
@@ -7,6 +7,7 @@ from __future__ import annotations
 from dataclasses import dataclass
 
 from .chunk import Chunk
+from .terrain_generator import TerrainGenerator
 
 
 @dataclass(slots=True)
@@ -21,3 +22,42 @@ class WorldMap:
     """
 
     generator: TerrainGenerator
+    """
+    Seeded generator used to produce base terrain.
+    """
+
+    _chunks: dict[tuple[int, int], Chunk]
+    """
+    Mapping from chunk coords (cx, cy) to generated chunks.
+    """
+
+    @classmethod
+    def new(cls, 
+            *, 
+            chunk_size: int, 
+            generator: TerrainGenerator) -> WorldMap:
+        """
+        Construct a new infinite world map.
+
+        Parameters
+        ----------
+        chunk_size : int
+            Width and height of each chunk (tiles).
+        generator : TerrainGenerator
+            Seeded generator used to produce terrain.
+
+        Returns
+        -------
+        WorldMap
+            A newly created WorldMap. 
+        """
+        return cls(
+            chunk_size=chunk_size,
+            generator=generator,
+            _chunks={}
+                )
+        pass
+
+    ## TODO CHANGE _chunks to an LRU cache
+    ## Then, make a renderer as before.
+    ## Then, instantiate everything including that nd table
